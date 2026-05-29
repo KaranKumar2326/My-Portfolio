576e883d559db85e734607b12a04d76bd7e9d98a
Karan Kumar <127759113+KaranKumar2326@users.noreply.github.com>
2026-05-29 12:48:30 +0530

    Enhance Footer with sponsor card and social links
    Added a sponsor card section to the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,55 +3,83 @@ import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
   AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
+      {/* Main Footer Row */}
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Developed by Karan</h3>
         </Col>
+
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} PS</h3>
         </Col>
+
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
                 href="https://github.com/KaranKumar2326"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://twitter.com/KaranKumar7107"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiOutlineTwitter />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/karankumar7107/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
               </a>
             </li>
           </ul>
+        </Col>
+      </Row>
+
+      {/* Sponsor Card Row */}
+      <Row className="justify-content-center mt-4">
+        <Col md="6">
+          <div
+            style={{
+              borderRadius: "12px",
+              overflow: "hidden",
+            }}
+          >
+            <iframe
+              src="https://github.com/sponsors/KaranKumar2326/card"
+              title="Sponsor KaranKumar2326"
+              height="225"
+              width="100%"
+              style={{
+                border: 0,
+                maxWidth: "100%",
+              }}
+            ></iframe>
+          </div>
         </Col>
       </Row>
     </Container>
